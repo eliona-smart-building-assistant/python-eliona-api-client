8ff0b6af4228af0cefe295cb34d74e0b84892204
Andreas Göbel <andreas.goebel@eliona.io>
2022-10-26 12:22:42 +0200

    change version to 2.2.3

--- generate.sh
+++ generate.sh
@@ -7,4 +7,4 @@ docker run --rm \
     --git-repo-id python-eliona-api-client \
     -i https://raw.githubusercontent.com/eliona-smart-building-assistant/eliona-api/develop/openapi.yaml \
     -o /local \
-    --additional-properties="packageName=eliona.api_client,projectName=Python Eliona API client,packageVersion=1.0.1"
+    --additional-properties="packageName=eliona.api_client,projectName=Python Eliona API client,packageVersion=2.2.3"
